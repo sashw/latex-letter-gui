0e456cb42d6df99a644f3dde51727ac6fab9de05
Sascha Wagner <sascha.wagner.89@gmail.com>
2015-09-22 23:10:40 +0200

    Core refactoring, use Letter class as package
    - Use the Letter class as a standalone Python module which can be imported
    - Wrote main.py which is the main test executable

--- letter.py
+++ letter.py
@@ -2,7 +2,7 @@ import sys, os
 from collections import OrderedDict
 from tempfile import TemporaryFile as tmp_file
 
-__version__ = 0.5
+__version__ = 0.6
 
 try:
     from latex import build_pdf, LatexBuildError
@@ -11,7 +11,6 @@ except ImportError:
     print("Please make sure to install it: pip install latex")
     print("with enough rights to install something.")
     sys.exit(1)
-
 
 class Letter:
     def __init__(self):
@@ -233,72 +232,3 @@ class Letter:
         for i, j in replace.items():
             text = text.replace(i, j)
         return text
-
-
-
-letter = Letter()
-letter.set_absender("John Doe", "Straße der Freiheit", "Berlin")
-text = "This is a line with a lot of symbols in it: % & _ § € ~ ^ \ | £ ° ™ © ¡ äß?\n"
-text = letter.replace_symbols_latex(text)
-letter.set_text(["\n", "\\begin{document}\n", "\\begin{g-brief}\n", "Text bla blub.\n", text, "\end{g-brief}\n", "\end{document}\n"])
-letter.save_tex('test_out.tex')
-letter.create_pdf()
-sys.exit(0)
-
-
-'''
-f = tmp_file()
-f.write(b"\documentclass{article}\n")
-f.write(b"\\usepackage[utf8]{inputenc}\n")
-f.write(b"\\usepackage[ngerman]{babel}\n")
-f.write(b"\\usepackage{gensymb}\n")
-f.write(b"\\usepackage{eurosym}\n")
-f.write(b"\\begin{document}\n")
-f.write(b"Hello, world!\n")
-f.write(b"\end{document}")
-f.seek(0)
-print(f.read().decode('utf-8'))
-'''
-
-
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QGridLayout, QLabel, QLineEdit
-from PyQt5.QtWidgets import QTextEdit, QWidget, QDialog, QApplication, QMainWindow
-# example: https://misperious.wordpress.com/2014/08/17/python-pyqt5-example/
-#TODO: check what really needs to be imported
-
-from letter_ui import Ui_MainWindow as gui
-
-class MainWindow(QMainWindow, gui):
-    def __init__(self, parent=None):
-        #QMainWindow.__init__(self, parent)
-        super(MainWindow, self).__init__(parent)
-        self.setupUi(self)
-
-app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
-sys.exit(app.exec_())
-
-
-#TODO: maybe include something like pdf.is_available() to check if latex is installed and can be used  (http://pythonhosted.org/latex/)
-#if not pdf.is_available(): ...
-
-'''
-try:
-    f.seek(0)
-    pdf = build_pdf(f.read().decode('utf-8'))
-    pdf.save_to('letter.pdf')
-except LatexBuildError as e:
-    for error in e.get_errors():
-        print(u'Error in {0[filename]}, line {0[line]}: {0[error]}'.format(error))
-        # also print one line of context
-        print(u'    {}'.format(error['context'][1]))
-        print()
-    sys.exit("Building pdf failed!")
-
-f.close()  # temporary file is automatically deleted here
-'''
-
-sys.exit()
---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import sys, os
+from letter import Letter
+
+from letter_ui import Ui_MainWindow as gui
+
+
+#TODO: save/create button filedialog öffnen http://doc.qt.io/qt-5/qfiledialog.html
+
+letter = Letter()
+letter.set_absender("John Doe", "Straße der Freiheit", "Berlin")
+text = "This is a line with a lot of symbols in it: % & _ § € ~ ^ \ | £ ° ™ © ¡ äß?\\\n"
+text = letter.replace_symbols_latex(text)
+letter.set_text(["\n", "\\begin{document}\n", "\\begin{g-brief}\n", "Text bla blub.\n", text, "\end{g-brief}\n", "\end{document}\n"])
+letter.save_tex('test_out.tex')
+letter.create_pdf()
+sys.exit(0)
+
+
+'''
+f = tmp_file()
+f.write(b"\documentclass{article}\n")
+f.write(b"\\usepackage[utf8]{inputenc}\n")
+f.write(b"\\usepackage[ngerman]{babel}\n")
+f.write(b"\\usepackage{gensymb}\n")
+f.write(b"\\usepackage{eurosym}\n")
+f.write(b"\\begin{document}\n")
+f.write(b"Hello, world!\\\\\n")
+text = "This is a line with a lot of symbols in it: % & _ § € ~ ^ \ | £ ° ™ © ¡ äß?\n"
+text = replace_symbols_latex(text)
+f.write(text.encode('utf-8'))
+f.write(b"\end{document}")
+f.seek(0)
+print(f.read().decode('utf-8'))
+'''
+
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QGridLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QTextEdit, QWidget, QDialog, QApplication, QMainWindow
+# example: https://misperious.wordpress.com/2014/08/17/python-pyqt5-example/
+#TODO: check what really needs to be imported
+
+class MainWindow(QMainWindow, gui):
+    def __init__(self, parent=None):
+        #QMainWindow.__init__(self, parent)
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+sys.exit(app.exec_())
+
+#TODO: maybe include something like pdf.is_available() to check if latex is installed and can be used  (http://pythonhosted.org/latex/)
+#if not pdf.is_available(): ...
+
+'''
+try:
+    f.seek(0)
+    pdf = build_pdf(f.read().decode('utf-8'))
+    pdf.save_to('letter.pdf')
+except LatexBuildError as e:
+    for error in e.get_errors():
+        print(u'Error in {0[filename]}, line {0[line]}: {0[error]}'.format(error))
+        # also print one line of context
+        print(u'    {}'.format(error['context'][1]))
+        print()
+    sys.exit("Building pdf failed!")
+
+f.close()  # temporary file is automatically deleted here
+'''
+
+sys.exit()
