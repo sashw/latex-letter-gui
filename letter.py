dc2a206be6f606e2e2e8d400bf350dcd5fa43b43
Sascha Wagner <sascha.wagner.89@gmail.com>
2015-09-22 22:58:47 +0200

    Letter class created, PDF creation working

--- letter.py
+++ letter.py
@@ -2,7 +2,7 @@ import sys, os
 from collections import OrderedDict
 from tempfile import TemporaryFile as tmp_file
 
-__version__ = 0.2
+__version__ = 0.3
 
 try:
     from latex import build_pdf, LatexBuildError
@@ -13,6 +13,189 @@ except ImportError:
     sys.exit(1)
 
 
+class Letter:
+    def __init__(self):
+        self.tex = None
+
+        self.data = OrderedDict([
+            # allgemeine Einstellungen
+            ('lochermarke', ['\lochermarke', True]),
+            ('faltmarken', ['\\faltmarken', True]),
+            ('fenstermarken', ['\\fenstermarken', True]),
+            ('trennlinien', ['\\trennlinien', True]),
+            ('klassisch', ['\klassisch', False]),
+            ('unserzeichen', ['\\unserzeichen', False]),
+            # Korrespondenz
+            ('ihrzeichen', ['\IhrZeichen', '']),
+            ('ihrschreiben', ['\IhrSchreiben', '']),
+            ('meinzeichen', ['\MeinZeichen', '']),
+
+            # Absender-Adresse
+            ('name', ['\\Name', '']),
+            ('strasse', ['\Strasse', '']),
+            ('zusatz', ['\Zusatz', '']),
+            ('retouradresse', ['\RetourAdresse', '']),
+            ('ort', ['\Ort', '']),
+            ('land', ['\Land', '']),
+            # Kontaktinformationen
+            ('telefon', ['\Telefon', '']),
+            ('telefax', ['\Telefax', '']),
+            ('telex', ['\Telex', '']),
+            ('http', ['\HTTP', '']),
+            ('email', ['\EMail', '']),
+            # Bankverbindung
+            ('bank', ['\Bank', '']),
+            ('blz', ['\BLZ', '']),
+            ('konto', ['\Konto', '']),
+            # Anschrift
+            ('postvermerk', ['\Postvermerk', '']),
+            ('adresse', ['\Adresse', '']),
+            # Inhaltsbezogen
+            ('datum', ['\Datum', '']),
+            ('betreff', ['\Betreff', '']),
+            ('anrede', ['\Anrede', '']),
+            ('gruss', ['\Gruss', '']),
+            ('unterschrift', ['\\Unterschrift', '']),
+
+            ('anlagen', ['\Anlagen', []]),
+            ('verteiler', ['\Verteiler', '']),
+
+            # eigentlicher Brieftext, begin document
+            ('text', ['text', []]),
+            ])
+
+    def set_val(self, name, val, idx=1):
+        self.data[name][idx] = val
+
+    def set_lochermarke(self, val):
+        self.set_val('lochermarke', val)
+
+    def set_faltmarken(self, val):
+        self.set_val('faltmarken', val)
+
+    def set_fenstermarken(self, val):
+        self.set_val('fenstermarken', val)
+
+    def set_trennlienen(self, val):
+        self.set_val('trennlienen', val)
+
+    def set_klassisch(self, val):
+        self.set_val('klassisch', val)
+
+    def set_unserzeichen(self, val):
+        self.set_val('unserzeichen', val)
+
+    def set_absender(self, name, strasse, ort, land='', zusatz='', retour=''):
+        self.set_val('name', name)
+        self.set_val('strasse', strasse)
+        self.set_val('ort', ort)
+        self.set_val('land', land)
+        self.set_val('zusatz', zusatz)
+        self.set_val('retouradresse', retour)
+
+    def set_ihrschreiben(self, datum):
+        self.set_val('ihrschreiben', datum)
+
+    def set_zeichen(self, ihrzeichen, meinzeichen):
+        self.set_val('ihrzeichen', ihrzeichen)
+        self.set_val('meinzeichen', meinzeichen)
+
+    def set_bank(self, bank, blz, konto):
+        self.set_val('bank', bank)
+        self.set_val('blz', blz)
+        self.set_val('konto', konto)
+
+    def set_tel(self, telefon, telefax='', telex=''):
+        self.set_val('telefon', telefon)
+        self.set_val('telefax', telefax)
+        self.set_val('telex', telex)
+
+    def set_mail(self, mail):
+        self.set_val('email', mail)
+
+    def set_homepage(self, web):
+        self.set_val('http', web)
+
+    def set_vermerk(self, vermerk):
+        self.set_val('postvermerk', vermerk)
+
+    def set_adresse(self, adresse):
+        self.set_val('adresse', adresse)
+
+    def set_betreff(self, betreff):
+        self.set_val('betreff', betreff)
+
+    def set_anrede(self, anrede):
+        self.set_val('anrede', anrede)
+
+    def set_gruss(self, gruss):
+        self.set_val('gruss', gruss)
+
+    def set_unterschrift(self, unterschrift):
+        self.set_val('unterschrift', unterschrift)
+
+    def set_anlagen(self, anlagen):
+        self.set_val('anlagen', anlagen)
+
+    def set_verteiler(self, verteiler):
+        self.set_val('verteiler', verteiler)
+
+    def set_text(self, text):
+        self.set_val('text', text)
+
+    def create_tex(self):
+        if self.tex is not None:
+            self.tex.close()
+
+        self.tex = tmp_file()
+
+        preamble = ['\documentclass[11pt]{g-brief}\n',
+                '\\usepackage[utf8]{inputenc}\n',
+                '\\usepackage[ngerman]{babel}\n',
+                '\\usepackage{enumerate}\n',
+                '\\usepackage{gensymb}\n',
+                '\\usepackage{eurosym}\n\n']
+
+        self.tex.writelines(l.encode('utf-8') for l in preamble)
+        #for line in preamble:
+        #    self.tex.write(line.encode('utf-8'))
+
+        for values in self.data.values():
+            if type(values[1]) is list:
+                self.tex.writelines(line.encode('utf-8') for line in values[1])
+            else:
+                line = ''
+                if type(values[1]) is bool:
+                    if values[1]:
+                        line = values[0]
+                    else:
+                        line = '%' + values[0]
+                else:
+                    line = '%s{%s}' % tuple(values)
+                    if '\Gruss' in values[0]:
+                        line += '{1cm}'
+                line += '\n'
+                self.tex.write(line.encode('utf-8'))
+        self.tex.write('\endinput'.encode('utf-8'))
+
+    def create_pdf(self, filename=''):
+        if not self.tex:
+            self.create_tex()
+        if not filename:
+            filename = 'letter.pdf'
+        self.tex.seek(0)
+        pdf = build_pdf(self.tex.read().decode('utf-8'))
+        pdf.save_to(filename)
+
+
+letter = Letter()
+letter.set_absender("John Doe", "Straße der Freiheit", "Berlin")
+letter.set_text(["\n", "\\begin{document}\n", "\\begin{g-brief}\n", "Text bla blub.\n", "\end{g-brief}\n", "\end{document}\n"])
+letter.create_pdf()
+sys.exit(0)
+
+
+'''
 f = tmp_file()
 f.write(b"\documentclass{article}\n")
 f.write(b"\\usepackage[utf8]{inputenc}\n")
@@ -24,7 +207,7 @@ f.write(b"Hello, world!\n")
 f.write(b"\end{document}")
 f.seek(0)
 print(f.read().decode('utf-8'))
-
+'''
 
 
 from PyQt5.QtCore import Qt
@@ -51,6 +234,7 @@ sys.exit(app.exec_())
 #TODO: maybe include something like pdf.is_available() to check if latex is installed and can be used  (http://pythonhosted.org/latex/)
 #if not pdf.is_available(): ...
 
+'''
 try:
     f.seek(0)
     pdf = build_pdf(f.read().decode('utf-8'))
@@ -64,6 +248,6 @@ except LatexBuildError as e:
     sys.exit("Building pdf failed!")
 
 f.close()  # temporary file is automatically deleted here
-
+'''
 
 sys.exit()
