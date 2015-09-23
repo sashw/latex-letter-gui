67be3e08cd51041ee9231946193993febe83ca53
Sascha Wagner <sascha.wagner.89@gmail.com>
2015-09-24 01:29:52 +0200

    Improved tests, including ImportError, some fixes
    - changed the way the setUp and tearDown functions were used (correct
      now)
    - implemented a way to test the ImportError if no latex module is
      available
    - refactoring and small changes

--- tests/test_letter.py
+++ tests/test_letter.py
@@ -9,59 +9,89 @@ Tests for `letter` module.
 """
 
 import unittest
-import shutil, tempfile
+import sys, shutil, tempfile
 from os.path import join as pjoin
-
-from letter.letter import Letter
+# use builtins to test ImportError
+try:
+    import builtins
+except ImportError:
+    import __builtin__ as builtins
 
 
 class TestLetter(unittest.TestCase):
 
-    def setUp(self):
-        self.test_dir = tempfile.mkdtemp()
-        self.tex = pjoin(self.test_dir, 'test_out.tex')
-        self.pdf = pjoin(self.test_dir, 'test_out.pdf')
-        self.letter = Letter()
-        self.letter.set_absender("John Doe", "Straße der Freiheit", "Berlin")
-        self.letter.set_adresse("Klaus Störtebeker\\\\Hamburg")
-        self.letter.set_lochermarke(True)
-        self.letter.set_faltmarken(True)
-        self.letter.set_fenstermarken(True)
-        self.letter.set_trennlinien(True)
-        self.letter.set_klassisch(False)
-        self.letter.set_unserzeichen(False)
-        self.letter.set_ihrschreiben("1.1.1970")
-        self.letter.set_zeichen("abc", "xyz")
-        self.letter.set_bank("Top Bank", 12345678, "0123456789")
-        self.letter.set_tel("+00 12345 67890567")
-        self.letter.set_mail("me@mail.tld")
-        self.letter.set_homepage("http://domain.tld")
-        self.letter.set_vermerk("")
-        self.letter.set_betreff("Superwichtige Mitteilung")
-        self.letter.set_anrede("Hallo")
-        self.letter.set_gruss("Gruß")
-        self.letter.set_unterschrift("John Doe")
-        self.letter.set_anlagen("")
-        self.letter.set_verteiler("")
-        text = "This is a line with a lot of symbols in it: % & _ § € ~ ^ \ | £ ° ™ © ¡ äß?\\\\\n"
-        text = self.letter.replace_symbols_latex(text)
-        self.letter.set_text(["\n", "\\begin{document}\n", "\\begin{g-brief}\n", "Text bla blub.\\\\\n", text, "\end{g-brief}\n", "\end{document}\n"])
+    @classmethod
+    def setUpClass(cls):
+        cls.test_dir = tempfile.mkdtemp()
+        cls.tex = pjoin(cls.test_dir, 'some/useless/dirs/test_out.tex')
+        cls.pdf = pjoin(cls.test_dir, 'test_out.pdf')
+
+    def test_00_import_error(self):
+        # construct a method which let the import of the latex package fail
+        self.original_import = builtins.__import__
+        def fail_import(name, *args, **kwargs):
+            if name is 'latex':
+                raise ImportError
+            return self.original_import(name, *args, **kwargs)
+        builtins.__import__ = fail_import
+
+        with self.assertRaises(SystemExit) as exc:
+            from letter.letter import Letter
+        self.assertEqual(exc.exception.code, 1)
+
+        builtins.__import__ = self.original_import
 
     def test_context_manager(self):
+        from letter.letter import Letter
         with Letter() as letter:
             pass
 
     def test_tex(self):
-        ret = self.letter.save_tex(self.tex)
+        from letter.letter import Letter
+        letter = Letter()
+        letter.set_text(["\n", "\\begin{document}\n", "\\begin{g-brief}\n", "Content.\n", "\end{g-brief}\n", "\end{document}\n"])
+        ret = letter.save_tex(self.tex)
         self.assertIsNone(ret)
 
     def test_pdf(self):
-        ret = self.letter.create_pdf(self.pdf)
+        from letter.letter import Letter
+        letter = Letter()
+        letter.set_text(["\n", "\\begin{document}\n", "\\begin{g-brief}\n", "Content.\n", "\end{g-brief}\n", "\end{document}\n"])
+        ret = letter.create_pdf(self.pdf)
         self.assertIsNone(ret)
 
-    def tearDown(self):
-        self.letter.__exit__()
-        shutil.rmtree(self.test_dir)
+    def test_setters(self):
+        from letter.letter import Letter
+        letter = Letter()
+        letter.set_absender("John Doe", "Straße der Freiheit", "Berlin")
+        letter.set_adresse("Klaus Störtebeker\\\\Hamburg")
+        letter.set_lochermarke(True)
+        letter.set_faltmarken(True)
+        letter.set_fenstermarken(True)
+        letter.set_trennlinien(True)
+        letter.set_klassisch(False)
+        letter.set_unserzeichen(False)
+        letter.set_ihrschreiben("1.1.1970")
+        letter.set_zeichen("abc", "xyz")
+        letter.set_bank("Top Bank", 12345678, "0123456789")
+        letter.set_tel("+00 12345 67890567")
+        letter.set_mail("me@mail.tld")
+        letter.set_homepage("http://domain.tld")
+        letter.set_vermerk("")
+        letter.set_datum("21.12.2012")
+        letter.set_betreff("Superwichtige Mitteilung")
+        letter.set_anrede("Hallo")
+        letter.set_gruss("Gruß")
+        letter.set_unterschrift("John Doe")
+        letter.set_anlagen("")
+        letter.set_verteiler("")
+        text = "This is a line with a lot of symbols in it: % & _ § € ~ ^ \ | £ ° ™ © ¡ äß?\n"
+        text = letter.replace_symbols_latex(text)
+        letter.set_text(["\n", "\\begin{document}\n", "\\begin{g-brief}\n", "Text bla blub.\\\\\n", text, "\end{g-brief}\n", "\end{document}\n"])
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.test_dir)
 
 if __name__ == '__main__':
     unittest.main()
